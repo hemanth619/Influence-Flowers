5a83d0da29486e5ca0f0e47976f562d3c6c4e2c3
Sandeep Nadella <vnadell1@asu.edu>
2019-12-05 00:18:44 -0700

    some minor css changes and edge case handling

--- flowerapp/test_queries.sql
+++ flowerapp/test_queries.sql
@@ -10,6 +10,8 @@ SELECT DISTINCT(TitleType) FROM movies_data;
 
 SELECT DISTINCT(IsAdult) FROM movies_data;
 
+SELECT Directors,Genres, PrimaryTitle, averageRating FROM movies_data m, title_ratings t WHERE t.tconst=m.Tconst AND IsAdult=1 ORDER BY averageRating DESC;
+
 SELECT * FROM movies_data WHERE Directors LIKE '%\N%';
 
 SELECT StartYear,TitleType,m.Tconst,averageRating,numVotes FROM movies_data m,title_ratings t WHERE t.tconst=m.Tconst AND StartYear IS NOT NULL AND StartYear <> '' AND Genres LIKE '%Adventure%' AND Genres LIKE '%Comedy%' AND CAST(averageRating as float)>=1 AND CAST(averageRating as float)<=10 ORDER BY StartYear ASC;
@@ -18,7 +20,7 @@ SELECT * FROM name_basics LIMIT 60;
 
 SELECT MIN(StartYear), MAX(StartYear) FROM movies_data m WHERE StartYear IS NOT NULL AND StartYear <> '';
 
-SELECT nconst,primaryName FROM name_basics WHERE nconst='nm0628345';
+SELECT nconst,primaryName FROM name_basics WHERE nconst='nm1114166';
 
 CREATE INDEX PersonIDIndex ON name_basics(nconst);
 
